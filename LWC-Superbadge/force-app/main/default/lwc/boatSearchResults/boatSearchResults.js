3eb6a445eca29ebd427f78d5a44eb154be51af1d
Ryan Kramer <rkramer613@gmail.com>
2021-08-26 13:16:37 -0600

    boatSearchResults component complete and passed

--- LWC-Superbadge/force-app/main/default/lwc/boatSearchResults/boatSearchResults.js
+++ LWC-Superbadge/force-app/main/default/lwc/boatSearchResults/boatSearchResults.js
@@ -1,4 +1,4 @@
-// ...
+
 import { LightningElement, wire, api, track } from "lwc";
 import getBoats from "@salesforce/apex/BoatDataService.getBoats";
 import { updateRecord } from "lightning/uiRecordApi";
@@ -6,6 +6,7 @@ import { ShowToastEvent } from "lightning/platformShowToastEvent";
 import { refreshApex } from '@salesforce/apex';
 import { publish, MessageContext } from "lightning/messageService";
 import BOATMC from '@salesforce/messageChannel/BoatMessageChannel__c';
+import updateBoatList from "@salesforce/apex/BoatDataService.updateBoatList";
 
 const SUCCESS_TITLE = 'Success';
 const MESSAGE_SHIP_IT     = 'Ship it!';
@@ -14,49 +15,125 @@ const ERROR_TITLE   = 'Error';
 const ERROR_VARIANT = 'error';
 
 export default class BoatSearchResults extends LightningElement {
-  selectedBoatId;
+
+  selectedBoatId = '';
   columns = [];
   boatTypeId = '';
   @track boats;
   isLoading = false;
-  @track draftValues;
+  @track draftValues = [];
+  error = undefined;
+  wiredBoatsResult;
   
   // wired message context
   @wire(MessageContext) messageContext;
 
-  // wired getBoats method 
-  wiredBoats(result) { }
-  
-  // public function that updates the existing boatTypeId property
+  columns = [
+    {label: 'Name', fieldName: 'Name', type: 'text', editable: 'true'},
+    {label: 'Length', fieldName: 'Length__c', type: 'number', editable: 'true'},
+    {label: 'Price', fieldName: 'Price__c', type: 'currency', editable: 'true'},
+    {label: 'Description', fieldName: 'Description__c', type: 'text', editable: 'true' }
+  ];
+
+   // public function that updates the existing boatTypeId property
   // uses notifyLoading
-  searchBoats(boatTypeId) { }
-  
-  // this public function must refresh the boats asynchronously
-  // uses notifyLoading
-  refresh() { }
-  
-  // this function must update selectedBoatId and call sendMessageService
-  updateSelectedTile() { }
-  
-  // Publishes the selected boat Id on the BoatMC.
-  sendMessageService(boatId) { 
-    // explicitly pass boatId to the parameter recordId
+  @api
+  searchBoats(boatTypeId) { 
+    this.isLoading = true;
+    this.notifyLoading(this.isLoading);
+    this.boatTypeId = boatTypeId;
   }
+
+  // wired getBoats method 
+  @wire(getBoats, {boatTypeId: '$boatTypeId' })
+    wiredBoats(result) {
+      this.boats = result;
+        if(result.error) {
+          this.error = result.error;
+          this.boats = undefined;
+        }
+        this.isLoading = false;
+        this.notifyLoading(this.isLoading);
+     }
+
+  // this function must update selectedBoatId and call sendMessageService
+  updateSelectedTile(event) {
+    this.selectedBoatId = event.detail.boatId;
+    this.sendMessageService(this.selectedBoatId);
+
+   }
   
-  // The handleSave method must save the changes in the Boat Editor
+   // The handleSave method must save the changes in the Boat Editor
   // passing the updated fields from draftValues to the 
   // Apex method updateBoatList(Object data).
   // Show a toast message with the title
   // clear lightning-datatable draft values
   handleSave(event) {
     // notify loading
-    const updatedFields = event.detail.draftValues;
-    // Update the records via Apex
-    updateBoatList({data: updatedFields})
-    .then(() => {})
-    .catch(error => {})
-    .finally(() => {});
+    this.notifyLoading(true);
+    const recordInputs = event.detail.draftValues.slice().map(draft => {
+      const fields = Object.assign({}, draft);
+      return {fields};
+    });
+
+    console.log(recordInputs);
+    const promises = recordInputs.map(recordInput => updateRecord(recordInput));
+    Promise.all(promises).then(res => {
+      this.dispatchEvent(
+        new ShowToastEvent({
+          title: SUCCESS_TITLE,
+          message: MESSAGE_SHIP_IT,
+          variant: SUCCESS_VARIANT
+        })
+      );
+      this.draftValues = [];
+      return this.refresh();
+    }).catch(error => {
+      this.error = error;
+      this.dispatchEvent(
+        new ShowToastEvent({
+          title: ERROR_TITLE,
+          message: CONST_ERROR,
+          variant: ERROR_VARIANT
+        })
+      );
+      this.notifyLoading(false);
+    }).finally(() => {
+      this.draftValues = [];
+    });
+
   }
-  // Check the current value of isLoading before dispatching the doneloading or loading custom event
-  notifyLoading(isLoading) { }
+  
+  // this public function must refresh the boats asynchronously
+  // uses notifyLoading
+  @api
+  async refresh() {
+    this.isLoading = true;
+    this.notifyLoading(this.isLoading);
+    await refreshApex(this.boats);
+    this.isLoading = false;
+    this.notifyLoading(this.isLoading);
+   }
+
+    // Check the current value of isLoading before dispatching the doneloading or loading custom event
+    notifyLoading(isLoading) { 
+      if(isLoading) {
+        this.dispatchEvent(new CustomEvent('loading'));
+      } else {
+        this.dispatchEvent(new CustomEvent('doneloading'));
+      }
+    }
+  
+  // Publishes the selected boat Id on the BoatMC.
+  sendMessageService(boatId) { 
+    // explicitly pass boatId to the parameter recordId
+    publish(this.messageContext, BOATMC, { recordId : boatId });
+  }
+
+
+
+
 }
+  
+
+
